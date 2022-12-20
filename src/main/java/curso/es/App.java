ff5a311bc74058230e1f8818447a83730e627bf3
Ubuntu <ubuntu@ip-172-31-43-200.eu-west-1.compute.internal>
2022-12-20 20:12:54 +0000

    jacoco

--- src/main/java/curso/es/App.java
+++ src/main/java/curso/es/App.java
@@ -15,4 +15,12 @@ public class App
     public static double Doblar(double NUMERO) {
         return NUMERO*2;
     }
+    
+    public static double Triplar(double numero) {
+        return numero*3;
+    }
+    
+    public static double Mitad(double numero) {
+        return numero/4;
+    }
 }
